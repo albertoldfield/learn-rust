ad987999d53aa9cf36fa892130c7a93be49c6eb1
Albert Oldfield <albert.oldfield@gmail.com>
2023-07-13 14:26:18 -0500

    .gitignore is now working

--- the-book/guessing_game/target/debug/deps/guessing_game-bdd25e83f789e2d1.d
+++
@@ -1,5 +0,0 @@
-/Users/albertoldfield/Documents/Code/sandbox/learn-rust/the-book/guessing_game/target/debug/deps/guessing_game-bdd25e83f789e2d1.rmeta: src/main.rs
-
-/Users/albertoldfield/Documents/Code/sandbox/learn-rust/the-book/guessing_game/target/debug/deps/guessing_game-bdd25e83f789e2d1.d: src/main.rs
-
-src/main.rs:
--- the-book/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/albertoldfield/Documents/Code/sandbox/learn-rust/the-book/guessing_game/target/debug/guessing_game: /Users/albertoldfield/Documents/Code/sandbox/learn-rust/the-book/guessing_game/src/main.rs
