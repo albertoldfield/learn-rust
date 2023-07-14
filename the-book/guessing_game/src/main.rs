2274da47fb08e1abcdef91b88fa71d9cca6663b3
Albert Oldfield <albert.oldfield@gmail.com>
2023-07-13 20:43:45 -0500

    Completed guessing_game exercise.

--- the-book/guessing_game/src/main.rs
+++ the-book/guessing_game/src/main.rs
@@ -1,13 +1,38 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Pick a number!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline!");
+    println!("The secret number is {secret_number}");
 
-    println!("You guessed {guess}");
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess.");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
